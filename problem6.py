016185a9f22f0c320e84b0efdfb7e60b0882fe6b
Mohammad Yamin <107704708+yaminyann@users.noreply.github.com>
2023-07-21 15:39:32 +0600

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,6 @@
+#problem link - https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (0,n):
+        print(i*i)
