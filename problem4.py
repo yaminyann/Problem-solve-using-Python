a80e2959357944ab22a8275e9d17717e61666627
Mohammad Yamin <107704708+yaminyann@users.noreply.github.com>
2023-07-21 11:57:58 +0600

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,8 @@
+# problem link - https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    item = 1
+    while item <= n:
+        print(item, end="")
+        item = item + 1
