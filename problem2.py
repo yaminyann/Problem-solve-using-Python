e4e8196b1d8f96487afe638eabefce8f5c99abea
Mohammad Yamin <107704708+yaminyann@users.noreply.github.com>
2023-07-21 11:14:35 +0600

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,10 @@
+#problem link - https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    c = a//b
+    d = a/b
+    print(c)
+    print(d)
