f225920918bf5e519d85657d20cf87fc7579dcb7
Md. Yamin Ali <107704708+yaminyann@users.noreply.github.com>
2025-05-06 22:28:01 +0600

    new

---
+++ problem_8.py
@@ -0,0 +1,14 @@
+
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums , target ):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+    
+obj = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+print(obj.twoSum(nums, target))
